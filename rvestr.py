8b08e96e759938787a396f59a7ccaf7f5ca93919
HarshKorade <harshkorade03@gmail.com>
2025-04-13 17:26:24 +0530

    Add files via upload
    code to reverse a given string

---
+++ rvestr.py
@@ -0,0 +1,16 @@
+class usermaincode():
+    @classmethod
+    def revstr(cls,str):
+        strnew = ""
+        for i in str:
+            strnew = i + strnew
+        print(strnew)
+nb = "k\nj\""
+
+
+new = "qbc123"
+a = usermaincode()
+print(a.revstr(new))
+
+
+
